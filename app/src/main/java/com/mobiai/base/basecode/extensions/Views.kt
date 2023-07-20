8c4384db29011e68ef3b346eecf2a9473036b186
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-07-20 22:45:45 +0700

    UI OnBoardingActivity

--- app/src/main/java/com/mobiai/base/basecode/extensions/Views.kt
+++ app/src/main/java/com/mobiai/base/basecode/extensions/Views.kt
@@ -1,10 +1,48 @@
-package com.base.lqlbasecode.extensions
+package com.mobiai.base.basecode.extensions
 
 import android.widget.ScrollView
+import android.view.View
 
 fun ScrollView.scrollToBottom() {
     val lastChild = getChildAt(childCount - 1)
     val bottom = lastChild.bottom + paddingBottom
     val delta = bottom - (scrollY+ height)
     smoothScrollBy(0, delta)
+}
+
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun View.isGone() : Boolean {
+    return this.visibility == View.GONE
+}
+
+fun View.isInvisible() : Boolean {
+    return this.visibility == View.INVISIBLE
+}
+
+fun View.isVisible() : Boolean {
+    return this.visibility == View.VISIBLE
+}
+
+fun getVisibleState() : Int {
+    return View.VISIBLE
+}
+
+fun getGoneState() : Int {
+    return View.GONE
+}
+
+fun getInvisibleState() : Int {
+    return View.INVISIBLE
 }
--- app/src/main/java/com/mobiai/base/basecode/storage/SharedPreferenceUtils.kt
+++ app/src/main/java/com/mobiai/base/basecode/storage/SharedPreferenceUtils.kt
@@ -1,10 +1,12 @@
 package com.mobiai.base.basecode.storage
 
+import com.mobiai.BuildConfig
 import com.mobiai.app.App
 
 object SharedPreferenceUtils {
     private const val FIRST_OPEN_APP = "FIRST_OPEN_APP"
     private const val LANGUAGE = "LANGUAGE"
+    private const val IS_COMPLETE_ONBOARDING = "IS_COMPLETE_ONBOARDING"
 
     var firstOpenApp: Boolean
         get() = App.instanceSharePreference.getValueBool(FIRST_OPEN_APP, true)
@@ -13,5 +15,12 @@ object SharedPreferenceUtils {
     var languageCode: String?
         get() = App.instanceSharePreference.getValue(LANGUAGE, null)
         set(value) = App.instanceSharePreference.setValue(LANGUAGE, value)
+
+    var isCompleteOnboarding : Boolean
+        get() =
+            if(BuildConfig.DEBUG) false
+            else
+                App.instanceSharePreference.getValueBool(IS_COMPLETE_ONBOARDING, false)
+        set(value) = App.instanceSharePreference.setValueBool(IS_COMPLETE_ONBOARDING, value)
 
 }
